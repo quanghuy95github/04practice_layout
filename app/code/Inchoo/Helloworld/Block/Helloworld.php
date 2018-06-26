5cdb3f5963a71b0a17e7b36ca1f8c713c6764c9f
Nguyen Quang Huy <quanghuy.tb.vn@gmail.com>
2018-06-27 05:42:13 +0700

    tim hieu ve xu layout

---
+++ app/code/Inchoo/Helloworld/Block/Helloworld.php
@@ -0,0 +1,23 @@
+<?php
+namespace Inchoo\Helloworld\Block;
+ 
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+
+    public function _beforeToHtml()
+    {
+    	$config = $this->_scopeConfig;
+    	$phone = $config->getValue('general/store_information/phone');
+    	$email = $config->getValue('trans_email/ident_general/email');
+    	$facebook = $config->getValue('social/social_config/facebook');
+    	$twitter = $config->getValue('social/social_config/twitter');
+    	$this->setData('store_phone', $phone);
+    	$this->setData('store_email', $email);
+    	$this->setData('store_facebook', $facebook);
+    	$this->setData('store_twitter', $twitter);
+    }
+}
---
+++ app/code/Robin/Eshoper/Block/HeaderTop.php
@@ -0,0 +1,10 @@
+<?php
+namespace Robin\Eshoper\Block;
+ 
+class headerTop extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
